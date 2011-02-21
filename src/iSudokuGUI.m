bbba6721d18a0abedc3b6ce6a50e5a0e2761dd13
ivan.castro.c <ivan.castro.c@gmail.com>
2011-02-21 23:18:08 +0000

    Added implementation for the verify button

--- src/iSudokuGUI.m
+++ src/iSudokuGUI.m
@@ -22,7 +22,7 @@ function varargout = iSudokuGUI(varargin)
 
 % Edit the above text to modify the response to help iSudokuGUI
 
-% Last Modified by GUIDE v2.5 01-Feb-2011 12:35:40
+% Last Modified by GUIDE v2.5 21-Feb-2011 22:51:50
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -1974,6 +1974,7 @@ end
 
 set(handles.SolveBtn, 'Enable', 'on');
 set(handles.ClearBtn, 'Enable', 'on');
+set(handles.verifyBtn, 'Enable', 'on');
 
 % --- Executes on button press in SolveBtn.
 function SolveBtn_Callback(hObject, eventdata, handles)
@@ -1981,68 +1982,31 @@ function SolveBtn_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-invalid = 0;
-toSolveMatrix = zeros(9,9);
-hintMatrix = zeros(9,9); % This matrix holds the position of the hints 
-                         % or given numbers before solving the game
-for rowInd = 1:9
-    for colInd = 1:9
-        cName = ['c' num2str(rowInd) num2str(colInd)];
-        expr = ['cValue = get(handles.' cName ', ''String'');'];        
-        eval(expr);
-        
-        %Validate data correctness prior solving the game
-        %Only values from 1-9 should be entered, an empty cell is validq 
-        if (isempty(cValue) == 0)
-            if( isempty(str2num(cValue)) == 1)
-                msg = ['Non numeric value found at row: ' num2str(rowInd) ', column: ' num2str(colInd)];
-                h = msgbox(msg,'Error','error');
-                invalid = 1;
-                break;
-            elseif (length(cValue) ~= 1 ||...             
-                str2num(cValue) < 1 ||...
-                str2num(cValue) > 9 )
-                msg = ['Invalid input found at row: ' num2str(rowInd) ', column: ' num2str(colInd)];
-                h = msgbox(msg,'Error','error');
-                invalid = 1;
-                break;
-            end
-         
-            %Cell data is valid, add it to the matrix
-            toSolveMatrix(rowInd,colInd) = str2num(cValue);
-            hintMatrix(rowInd,colInd) = 1;
-        else
-            %Change the empty cell to zero and add it to the matrix
-            toSolveMatrix(rowInd,colInd) = 0;
-        end
-        
-    end
-    
-    if (invalid == 1)
-        break;
-    end
-end
+% First Read the board:
+[validMatrix toSolveMatrix hintMatrix] = readBoard(handles);
 
 % At this point toSolveMatrix holds the Sudoku game that needs to be
 % solved automatically.
 
-solvedMatrix = iSudokuALG(toSolveMatrix);
+if (validMatrix == 1)
+    solvedMatrix = iSudokuALG(toSolveMatrix);
 
-% Now it's time to populate the GUI with the solution obtained from
-% the solving algorithm:
+    % Now it's time to populate the GUI with the solution obtained from
+    % the solving algorithm:
 
-for rowInd = 1:9
-    for colInd = 1:9
-        cName = ['c' num2str(rowInd) num2str(colInd)];
-        cValue = num2str(solvedMatrix(rowInd, colInd));
-        if (hintMatrix(rowInd,colInd) == 1)
-            % This position corresponds to a hint
-            expr = ['set(handles.' cName ', ''String'', ''' cValue ''')'];
-        else
-            % This position is part of the solution
-            expr = ['set(handles.' cName ', ''String'', ''' cValue ''', ''ForegroundColor'', [' num2str([1 0 0]) '], ''Enable'', ''inactive'')'];
+    for rowInd = 1:9
+        for colInd = 1:9
+            cName = ['c' num2str(rowInd) num2str(colInd)];
+            cValue = num2str(solvedMatrix(rowInd, colInd));
+            if (hintMatrix(rowInd,colInd) == 1)
+                % This position corresponds to a hint
+                expr = ['set(handles.' cName ', ''String'', ''' cValue ''')'];
+            else
+                % This position is part of the solution
+                expr = ['set(handles.' cName ', ''String'', ''' cValue ''', ''ForegroundColor'', [' num2str([1 0 0]) '], ''Enable'', ''inactive'')'];
+            end
+            eval(expr);
         end
-        eval(expr);
     end
 end
 %set(handles.SolveBtn, 'Enable', 'off');
@@ -2084,3 +2048,86 @@ for rowInd = 1:9
 end
 
 set(handles.SolveBtn, 'Enable', 'off');
+set(handles.verifyBtn, 'Enable', 'off');
+
+% --- Executes on button press in verifyBtn.
+function verifyBtn_Callback(hObject, eventdata, handles)
+% hObject    handle to verifyBtn (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% First Read the board:
+[validMatrix toSolveMatrix hintMatrix] = readBoard(handles);
+numberOfHints = sum(sum(hintMatrix));
+
+if (validMatrix == 1)
+    % Verify that the solution / partial solution is correct
+    incorrectGame = verific(toSolveMatrix);
+    
+    if (all(all(hintMatrix)) == 1 ) % Complete Game
+        if (incorrectGame == 1)
+            msg = 'Solution is incorrect';
+            h = msgbox(msg,'Error','error', 'replace');
+        else
+            msg = 'Solution is Correct!, Game Completed';
+            h = msgbox(msg,'Correct','help', 'replace');
+        end
+    else % Partial Game
+        if (incorrectGame == 1)
+            msg = 'Partial Solution is incorrect';
+            h = msgbox(msg,'Error','error', 'replace');
+        else
+            toGo= 81 - numberOfHints;
+            msg = ['Partial Solution is Correct!, Empty Cells: ' num2str(toGo)];
+            h = msgbox(msg,'Correct','help', 'replace');
+        end
+    end
+end
+        
+
+% This function reads the board and generates a numeric matrix with the
+% game and a hint matrix
+function [valid toSolveMatrix hintMatrix] = readBoard(handles)
+
+valid = 1;
+toSolveMatrix = zeros(9,9);
+hintMatrix = zeros(9,9); % This matrix holds the position of the hints 
+                         % or given numbers before solving the game
+for rowInd = 1:9
+    for colInd = 1:9
+        cName = ['c' num2str(rowInd) num2str(colInd)];
+        expr = ['cValue = get(handles.' cName ', ''String'');'];        
+        eval(expr);
+        
+        %Validate data correctness prior solving the game
+        %Only values from 1-9 should be entered, an empty cell is valid 
+        if (isempty(cValue) == 0)
+            if( isempty(str2num(cValue)) == 1)
+                msg = ['Non numeric value found at row: ' num2str(rowInd) ', column: ' num2str(colInd)];
+                h = msgbox(msg,'Error','error', 'replace');
+                valid = 0;
+                break;
+            elseif (length(cValue) ~= 1 ||...             
+                str2num(cValue) < 1 ||...
+                str2num(cValue) > 9 )
+                msg = ['Invalid input found at row: ' num2str(rowInd) ', column: ' num2str(colInd)];
+                h = msgbox(msg,'Error','error', 'replace');
+                valid = 0;
+                break;
+            end
+         
+            %Cell data is valid, add it to the matrix
+            toSolveMatrix(rowInd,colInd) = str2num(cValue);
+            hintMatrix(rowInd,colInd) = 1;
+        else
+            %Change the empty cell to zero and add it to the matrix
+            toSolveMatrix(rowInd,colInd) = 0;
+        end
+        
+    end
+    
+    if (valid == 0)
+        break;
+    end
+end
+
